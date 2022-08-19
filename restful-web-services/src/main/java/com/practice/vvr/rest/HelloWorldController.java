5285cda79a9912e620c0b1ca96e4424782655377
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-08-19 11:31:31 +0530

    Logging level, beans, path variable

--- restful-web-services/src/main/java/com/practice/vvr/rest/HelloWorldController.java
+++ restful-web-services/src/main/java/com/practice/vvr/rest/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.practice.vvr.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,16 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
 }
