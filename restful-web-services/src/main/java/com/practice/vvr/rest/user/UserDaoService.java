86fc6c382cb81de5f05943e281de7eebc140e468
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-09-13 20:32:11 +0530

    User GET, POST methods and Log file

--- restful-web-services/src/main/java/com/practice/vvr/rest/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/practice/vvr/rest/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.practice.vvr.rest.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -11,14 +12,28 @@ public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
 	
+	private static Integer usersCount = 0;
+	
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Adam", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Adam", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(20)));
 	}
 	
 	public List<User> findAll() {
 		return users;
+	}
+	
+	public User findOne(Integer id) {
+//		return users.get(id);
+		Predicate<? super User> predicate = users -> {return users.getId().equals(id);};
+		return users.stream().filter(predicate).findFirst().get();
+	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 
 }
---
+++ restful-web-services/src/main/java/com/practice/vvr/rest/user/UserResource.java
@@ -0,0 +1,36 @@
+package com.practice.vvr.rest.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	private UserDaoService service;
+	
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+	
+	//GET /users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable("id") Integer id) {
+		return service.findOne(id);
+	}
+
+	//POST /users
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		service.save(user);
+	}
+}
