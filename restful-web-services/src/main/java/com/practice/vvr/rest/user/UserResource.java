049940345338ad59c9931b642a5d210ee0ae4ead
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-09-23 10:25:48 +0530

    POST method with correct response code

--- restful-web-services/src/main/java/com/practice/vvr/rest/user/UserResource.java
+++ restful-web-services/src/main/java/com/practice/vvr/rest/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.practice.vvr.rest.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -17,21 +20,23 @@ public class UserResource {
 		this.service = service;
 	}
 	
-	//GET /users
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
 	
-	//GET /user/{id}
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable("id") Integer id) {
 		return service.findOne(id);
 	}
 
-	//POST /users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User saved = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+					   .path("/{id}")
+					   .buildAndExpand(saved.getId())
+					   .toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
