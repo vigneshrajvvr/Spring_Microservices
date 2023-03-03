d63135bc6d6c7f52a01bb6d69af4053b44c3723e
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-03-03 23:25:56 +0530

    Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/practice/vvr/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,19 @@
+package com.practice.vvr.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable("from") String from,
+			@PathVariable("to") String to) {
+		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+	}
+
+}
