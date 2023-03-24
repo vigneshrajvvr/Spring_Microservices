db34242f730d69c298a3cb4b303ae1bc366988d0
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-03-24 23:59:18 +0530

    jpa changes

--- currency-exchange-service/src/main/java/com/practice/vvr/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/practice/vvr/currencyexchangeservice/CurrencyExchangeController.java
@@ -22,6 +22,7 @@ public class CurrencyExchangeController {
 		CurrencyExchange currentExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
 		String port = environment.getProperty("local.server.port");
 		currentExchange.setEnviroment(port);
+		// need to add jpa changes
 		
 		return currentExchange;
 	}	
