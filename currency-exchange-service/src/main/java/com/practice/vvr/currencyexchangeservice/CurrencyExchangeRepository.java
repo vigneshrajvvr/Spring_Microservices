45f70d9b71c496910cbf6aa20d9c46582f47ec6c
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-03-25 23:57:47 +0530

    Added JPA repository

---
+++ currency-exchange-service/src/main/java/com/practice/vvr/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.practice.vvr.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Integer>{
+
+
+}
