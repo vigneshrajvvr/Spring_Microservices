d9b983d2f5c69ce665a78cca6843881dafd7c870
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-03-31 00:19:17 +0530

    Currency Conversion Service

---
+++ currency-conversion-service/src/main/java/com/practive/vvr/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,22 @@
+package com.practive.vvr.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+	
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversion(
+			@PathVariable("from") String from,
+			@PathVariable("to") String to,
+			@PathVariable("quantity") BigDecimal quantity
+			) {
+		
+		return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+	}
+
+}
