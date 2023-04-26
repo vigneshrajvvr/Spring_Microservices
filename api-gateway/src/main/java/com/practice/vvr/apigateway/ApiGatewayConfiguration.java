6351e960eccdaf4b11b51a455d3181a1323fd64e
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-04-26 11:51:04 +0530

    Custom routes and global filter in API gateway

--- api-gateway/src/main/java/com/practice/vvr/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/practice/vvr/apigateway/ApiGatewayConfiguration.java
@@ -11,7 +11,18 @@ public class ApiGatewayConfiguration {
 	@Bean
 	public RouteLocator	gatewayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
-					.route(p -> p.path("/get").uri("http://httpbin.org:80"))
+					.route(p -> p.path("/get")
+							.filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+										  .addRequestParameter("Param", "MyValue"))
+								.uri("http://httpbin.org:80"))
+					.route(p -> p.path("/currency-exchange/**")
+								.uri("lb://currency-exchange"))
+					.route(p -> p.path("/currency-conversion/**")
+							.uri("lb://currency-conversion"))
+					.route(p -> p.path("/currency-conversion-new/**")
+								.filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)", 
+															"/currency-conversion/${segment}"))
+								.uri("lb://currency-conversion"))
 					.build();
 	}
 
