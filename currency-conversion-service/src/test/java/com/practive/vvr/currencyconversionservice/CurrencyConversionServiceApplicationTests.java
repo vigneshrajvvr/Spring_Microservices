919974a6da8ff29fb2d7a54da37003bf7b004597
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-10-10 21:39:48 +0530

    Microservices

---
+++ currency-conversion-service/src/test/java/com/practive/vvr/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practive.vvr.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ naming-server/src/test/java/com/practice/vvr/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
