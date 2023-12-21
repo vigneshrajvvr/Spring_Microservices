aa7d9b08f4419bc7d352f079f85425e2c20b49bd
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-12-21 23:31:39 +0530

    Accounts and Customer mapper classes

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,26 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Accounts extends  BaseEntity{
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
