84ef4ec8154813f9b6a43230b69e34d5f25849e3
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2024-01-02 23:51:26 +0530

    POST - Create Customer and Global Exception Handling

--- section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -3,6 +3,9 @@ package com.eazybytes.accounts.controller;
 import com.eazybytes.accounts.constants.AccountConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +13,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces= MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
+
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
-
+        iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
--- section2/accounts/src/main/java/com/eazybytes/accounts/service/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/AccountsServiceImpl.java
@@ -1,11 +1,21 @@
 package com.eazybytes.accounts.service;
 
+import com.eazybytes.accounts.constants.AccountConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -15,8 +25,34 @@ public class AccountsServiceImpl implements IAccountsService{
 
     private CustomerRepository customerRepository;
 
+    /**
+     *
+     * @param customerDto - CustomerDto object
+     */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with give mobile number "
+             + customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
 
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Anonymous");
+        return newAccount;
     }
 }
