b7d923096e8664e1beb211b7bf7093ca996e51fd
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2024-01-30 19:15:07 +0530

    Validation of fields

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,15 +1,21 @@
 package com.eazybytes.accounts.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 
 }
