0efc0b0d3094de5765a107562e3a4f23ba5c93d8
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2024-01-31 22:34:32 +0530

    JPA Auditing

--- section2/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -2,11 +2,17 @@ package com.eazybytes.accounts.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Cleanup;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,17 +21,22 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
--- section2/accounts/src/main/java/com/eazybytes/accounts/service/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/AccountsServiceImpl.java
@@ -41,8 +41,6 @@ public class AccountsServiceImpl implements IAccountsService{
             throw new CustomerAlreadyExistsException("Customer already registered with give mobile number "
              + customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -55,8 +53,6 @@ public class AccountsServiceImpl implements IAccountsService{
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountConstants.SAVINGS);
         newAccount.setBranchAddress(AccountConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
